6fe253952f928037571d0e656676f60ffd71da00
flackery <andrushagalushka@gmail.com>
2018-07-04 07:50:06 +0300

    update common.js

--- common.js
+++ common.js
@@ -11,20 +11,20 @@ element.onclick = (e) => {
   buttonClose.innerHTML = 'X'
   const tagText = document.querySelector('#tag').value
   const coordinate = getMouseCoordinate(element, e)
-  if (currentTagText !== tagText) {
+  if (e.target.getAttribute('id') === 'f-div') {
     span.innerHTML = tagText
-    span.style.display = `block`
+    span.style.display = 'block'
     span.setAttribute('id', `tag${count}`)
-    span.setAttribute('onclick', 'showCloseButton(this)')
-    span.setAttribute('onmousedown', 'move(this, event)')
-    span.setAttribute('onmouseup', 'stopMove(this, event)')
+    span.addEventListener('click', event => {showCloseButton(span, event)}, false)
+    span.addEventListener('mousedown', event => {move(span, event)}, false)
+    span.addEventListener('mouseup', event => {stopMove(span, event)}, false)
     element.appendChild(span)
     if (coordinate.x < span.offsetWidth / 2 && coordinate.y < span.offsetHeight / 2) { // top-left corner
-      span.style.top = `${5}px`
-      span.style.left = `${5}px`
+      span.style.top = `5px`
+      span.style.left = `5px`
     } else if (coordinate.x + span.offsetWidth / 2 > element.offsetWidth && coordinate.y < span.offsetHeight / 2) { // top-right corner
       let outside = coordinate.x + span.offsetWidth / 2 - element.offsetWidth
-      span.style.top = `${5}px`
+      span.style.top = `5px`
       span.style.left = `${(coordinate.x - span.offsetWidth / 2) - (outside + 5)}px`
     } else if (coordinate.x + span.offsetWidth / 2 > element.offsetWidth && coordinate.y + span.offsetHeight / 2 > element.offsetHeight) { // bottom-right corner
       let outsideRight = coordinate.x + span.offsetWidth / 2 - element.offsetWidth
@@ -34,17 +34,16 @@ element.onclick = (e) => {
     } else if (coordinate.x < span.offsetWidth / 2 && coordinate.y + span.offsetHeight / 2 > element.offsetHeight) { // bottom-left corner
       let outsideBottom = coordinate.y + span.offsetHeight / 2 - element.offsetHeight
       span.style.top = `${coordinate.y - span.offsetHeight / 2 - (outsideBottom + 5)}px`
-      span.style.left = `${5}px`
+      span.style.left = `5px`
     } else if (coordinate.x < span.offsetWidth / 2) { // top border
-      let outside = coordinate.x - span.offsetWidth / 2
       span.style.top = `${coordinate.y - span.offsetHeight / 2}px`
-      span.style.left = `${5}px`
+      span.style.left = `5px`
     } else if (coordinate.x + span.offsetWidth / 2 > element.offsetWidth) { // bottom border
       let outside = coordinate.x + span.offsetWidth / 2 - element.offsetWidth
       span.style.top = `${coordinate.y - span.offsetHeight / 2}px`
       span.style.left = `${(coordinate.x - span.offsetWidth / 2) - (outside + 5)}px`
     } else if (coordinate.y < span.offsetHeight / 2) { // left border
-      span.style.top = `${5}px`
+      span.style.top = `5px`
       span.style.left = `${coordinate.x - span.offsetWidth / 2}px`
     } else if (coordinate.y + span.offsetHeight / 2 > element.offsetHeight) { // right border
       let outside = coordinate.y + span.offsetHeight / 2 - element.offsetHeight
@@ -59,18 +58,17 @@ element.onclick = (e) => {
     buttonClose.style.top = `0px`
     if (coordinate.x + (span.offsetWidth / 2) + 22 >= element.offsetWidth) {
       buttonClose.style.left = `-22px`
-      console.log('left')
     } else {
       buttonClose.style.right = `-22px`
-      console.log('right')
     }
-    buttonClose.setAttribute('onclick', `closeTag(this.getAttribute('id'))`)
+    buttonClose.addEventListener('click', event => {
+      closeTag(buttonClose.getAttribute('id'), event)
+    }, false)
     span.appendChild(buttonClose)
     currentTagText = tagText
-  } else {
-    console.log('This tag is already added!!!!')
   }
 }
+
 
 function getMouseCoordinate(element, e) {
   return {
@@ -89,7 +87,7 @@ function move (span, e) {
         span.style.top = `${(element.offsetHeight - 5) - span.offsetHeight}px`
         span.style.left = `${(element.offsetWidth - 5) - span.offsetWidth}px`
       } else if (coordinate.y - span.offsetHeight / 2 <= 5) {
-        span.style.top = `${5}px`
+        span.style.top = `5px`
         span.style.left = `${(element.offsetWidth - 5) - span.offsetWidth}px`
       } else {
         span.style.top = `${coordinate.y - span.offsetHeight / 2}px`
@@ -98,13 +96,13 @@ function move (span, e) {
     } else if (coordinate.x - span.offsetWidth / 2 <= 5) {
       if (coordinate.y + span.offsetHeight >= element.offsetHeight - 5) {
         span.style.top = `${(element.offsetHeight - 5) - span.offsetHeight}px`
-        span.style.left = `${5}px`
+        span.style.left = `5px`
       } else if (coordinate.y - span.offsetHeight / 2 <= 5) {
-        span.style.top = `${5}px`
-        span.style.left = `${5}px`
+        span.style.top = `5px`
+        span.style.left = `5px`
       } else {
         span.style.top = `${coordinate.y - span.offsetHeight / 2}px`
-        span.style.left = `${5}px`
+        span.style.left = `5px`
       }
     } else if (coordinate.y + span.offsetHeight >= element.offsetHeight - 5) {
       span.style.top = `${(element.offsetHeight - 5) - span.offsetHeight}px`
@@ -119,7 +117,7 @@ function move (span, e) {
         }
       }
     } else if (coordinate.y - span.offsetHeight / 2 <= 5) {
-      span.style.top = `${5}px`
+      span.style.top = `5px`
       span.style.left = `${coordinate.x - span.offsetWidth / 2}px`
       if (closeButton.style.display === 'block') {
         if (+closeButton.style.right.slice(0, 3) < 0 && coordinate.x + span.offsetWidth / 2 >= (element.offsetWidth - 10) - 23) {
@@ -150,26 +148,12 @@ function stopMove (span, e) {
   body.onmousemove = null
 }
 
-function showCloseButton (span) {
-  // let isVisible = span.childNodes[1].style.display !== 'none'
-  // if (!isVisible) {
-  //   document.getElementById(`close-${span.getAttribute('id')}`).style.display = `block`
-  //   isVisible = true
-  //   console.log('show')
-  // } else {
-  //   document.getElementById(`close-${span.getAttribute('id')}`).style.display = `none`
-  //   isVisible = false
-  //   console.log('close')
-  // }
-  //
-  // if (isVisible) {
-  //   span.removeAttribute('onclick')
-  // }
+function showCloseButton (span, e) {
   document.getElementById(`close-${span.getAttribute('id')}`).style.display = `block`
-  span.removeAttribute('onclick')
+  span.removeEventListener('click', event => {showCloseButton(span, event)})
 }
 
-function closeTag (id) {
+function closeTag (id, e) {
   const spanId = id.slice(6, id.length)
   document.getElementById(`${spanId}`).style.display = 'none'
 }
